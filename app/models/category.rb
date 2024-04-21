73b226387c0a128aeabb41fdce527c82e5a68000
Christopher Valle <christxpherv@gmail.com>
2024-04-21 10:53:19 -0500

    added two models with three attributes each

---
+++ app/models/category.rb
@@ -0,0 +1,4 @@
+# created a category model with the following attributes: name, description, and status
+
+class Category < ApplicationRecord
+end
---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+# created a product model with the following attributes: name, price, and description.
+
+class Product < ApplicationRecord
+end
