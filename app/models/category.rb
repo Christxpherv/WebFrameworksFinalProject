a2d36794b5a33f40aaf5a2d1bcc14ecd3e1101df
Christopher Valle <christxpherv@gmail.com>
2024-04-21 11:00:10 -0500

    added validation for the cateogry model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 # created a category model with the following attributes: name, description, and status
 class Category < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :status, inclusion: { in: [true, false] }
 end
