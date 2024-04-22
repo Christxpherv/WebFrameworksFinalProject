d1e034ff67b89e677175ced93ce08d9972f167b8
Christopher Valle <christxpherv@gmail.com>
2024-04-22 16:47:58 -0500

    updated the controller for the categories model to support server side sorting using the .order() method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    # use the order method to sort the categories by the column specified in the params hash
+    @categories = Category.order(params[:sort_by])
     render :index
   end
 end
