eb9900beffb6bd98a8557d56bb11c466a207e043
Christopher Valle <christxpherv@gmail.com>
2024-04-22 14:37:51 -0500

    created further validation for the string data type in the products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 # created a product model with the following attributes: name, price, and quantity
 class Product < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 0, maximum: 50 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 end
