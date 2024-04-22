67e2907e72d04933d361c4e36eede55059067441
Christopher Valle <christxpherv@gmail.com>
2024-04-22 14:38:49 -0500

    added further validation for strings in categories model to restrict the length of the string

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # created a category model with the following attributes: name, description, and status
 class Category < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 0, maximum: 50 }
   validates :description, presence: true
   validates :status, inclusion: { in: [true, false] }
 end
